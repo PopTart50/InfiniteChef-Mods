762b971ee29d0fe32d12129c9ea18e4587d5fceb
PopTart50 <132015856+PopTart50@users.noreply.github.com>
2024-05-16 09:24:49 -0500

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,15 @@
+addIngredient("stuff", {
+        shape:"rock_ball",
+        color:["#f5ef42","#f5e342"],
+        hidden:false
+    }
+);
+
+addIngredient("stuff", {
+        shape:"rock_ball",
+        color:["#c2c1b8","#8f8e89"],
+        hidden:false
+    }
+);
+
+addRecipe("stuff+salt","salted stuff!");
